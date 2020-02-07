290a3ddc3ff8c24ce0778650cd2033b2112db28c
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2020-02-07 11:44:39 +0900

    Change how to import font

--- src/lib/theme.js
+++ src/lib/theme.js
@@ -1,7 +1,5 @@
 import { createMuiTheme } from '@material-ui/core/styles'
 import { jaJP } from '@material-ui/core/locale'
-import KoruriTtf from 'fonts/Koruri-Regular.ttf'
-import AudiowideTtf from 'fonts/Audiowide-Regular.ttf'
 
 const palette = {
   primary: { main: '#00796B' },
@@ -12,47 +10,12 @@ const palette = {
   success: { main: '#26c6da' },
 }
 
-const koruri = {
-  fontFamily: 'Koruri',
-  fontStyle: 'normal',
-  fontDisplay: 'swap',
-  fontWeight: 400,
-  src: `
-    local('Koruri'),
-    local('Koruri-Regular'),
-    url(${KoruriTtf}) format('ttf')
-  `,
-  unicodeRange:
-    'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF',
-}
-
-const audiowide = {
-  fontFamily: 'Audiowide',
-  fontStyle: 'normal',
-  fontDisplay: 'swap',
-  fontWeight: 400,
-  src: `
-    local('Audiowide'),
-    local('Audiowide-Regular'),
-    url(${AudiowideTtf}) format('ttf')
-  `,
-  unicodeRange:
-    'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF',
-}
-
 const themeName = 'Hayato OKUMA Katagami annotation tool theme'
 
 const theme = createMuiTheme(
   {
     palette,
     themeName,
-    overrides: {
-      MuiCssBaseline: {
-        '@global': {
-          '@font-face': [koruri, audiowide],
-        },
-      },
-    },
   },
   jaJP
 )
@@ -84,11 +47,14 @@ theme.typography = {
     '&:hover': {
       color: theme.palette.secondary.main,
     },
+    [theme.breakpoints.down('xs')]: {
+      fontSize: 14,
+    },
   },
   h4: {
     fontFamily: 'Koruri',
     fontSize: 24,
-    fontWeight: 700,
+    fontWeight: 'bold',
     color: theme.palette.primary.main,
     [theme.breakpoints.down('xs')]: {
       fontSize: 20,
