5e715d467f9b55a1bb6b79fe38e34bbd67898807
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2020-02-07 09:11:06 +0900

    Fix route

--- src/App.js
+++ src/App.js
@@ -5,24 +5,30 @@ import theme from 'lib/theme'
 import Header from 'components/Header'
 import Hero from 'components/Hero'
 import About from 'sections/About'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 import Projects from 'sections/Projects'
 import Writing from 'sections/Writing'
 import Footer from 'components/Footer'
 
-const App = () => {
+const Main = () => {
   const isMobile = useMediaQuery('(max-width:600px)')
   return (
     <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Header {...{ isMobile }} />
-        <Hero {...{ isMobile }} />
-        <About {...{ isMobile }} />
-        <Projects {...{ isMobile }} />
-        <Writing {...{ isMobile }} />
-        <Footer {...{ isMobile }} />
-      </BrowserRouter>
+      <Header {...{ isMobile }} />
+      <Hero {...{ isMobile }} />
+      <About {...{ isMobile }} />
+      <Projects {...{ isMobile }} />
+      <Writing {...{ isMobile }} />
+      <Footer {...{ isMobile }} />
     </ThemeProvider>
+  )
+}
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Route path="/" component={Main} />
+    </BrowserRouter>
   )
 }
 
