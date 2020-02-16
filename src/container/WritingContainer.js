a946c9b710b6247f959cfc4730511a8d1b3b88eb
8810KMKM <hayato.okuma892@mail.kyutech.jp>
2020-02-17 01:12:07 +0900

    Fix

--- src/container/WritingContainer.js
+++ src/container/WritingContainer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSuspenseQuery } from 'react-fetching-library'
-import { fetchMyQiitaArticles } from '../api/fetchMyQiitaArticles'
+import { fetchMyQiitaArticles } from 'api/fetchMyQiitaArticles'
 import Writing from 'sections/Writing'
 
 export default () => {
